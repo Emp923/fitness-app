013a91f3bee72551b71fa7a40a4091c7f70aaf11
adpenrod <alanpenrod6@gmail.com>
2025-02-26 17:07:19 -0500

    program database

--- fitness-db/schema.sql
+++ fitness-db/schema.sql
@@ -24,4 +24,29 @@ CREATE TABLE user_details (
     CONSTRAINT FK_user_details FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS program_exercises CASCADE;
+
+CREATE TABLE program_exercises (
+    id SERIAL PRIMARY KEY,
+    day_of_the_week varchar(200),
+    exercise varchar(50),
+    resistance INT,
+    sets INT,
+    repetitions INT,
+    user_id INT,
+    CONSTRAINT FK_program_exercises FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS exercise_log CASCADE;
+
+CREATE TABLE exercise_log (
+    id SERIAL PRIMARY KEY,
+    recorded_date DATE,
+    resistance INT,
+    sets INT,
+    repetitions INT,
+    exercise_id INT,
+    CONSTRAINT FK_exercise_log FOREIGN KEY (exercise_id) REFERENCES program_exercises(id) ON DELETE CASCADE
+);
+
 COMMIT TRANSACTION;
