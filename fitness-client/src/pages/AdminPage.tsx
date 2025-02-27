a4bc559cc01d70d79c901a7589cbf0b52acac94c
Eric Penrod <emp923@gmail.com>
2025-02-27 13:04:30 -0500

    update user roles admin page

--- fitness-client/src/pages/AdminPage.tsx
+++ fitness-client/src/pages/AdminPage.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
-import { getUsers, User } from "../services/userService";
+import { getUsers, updateUserRole, User } from "../services/userService";
 
 const AdminPage = () => {
   const { token } = JSON.parse((useSelector((state:RootState) => state.auth)).userInfo || "");
   const [users, setUsers] = useState<User[]>([]);
 
+  const updateRole = async (id: number, role: string) => {
+    await updateUserRole(id, role, token);
+  };
+
   useEffect(() => {
-    const callApi = async (token: String) => {
+    const callApi = async (token: string) => {
       try {
         const users = await getUsers(token);
         setUsers(users);
@@ -18,7 +22,7 @@ const AdminPage = () => {
     };
 
     callApi(token);
-  }, [token]);
+  }, [token, updateRole]);
 
   return (
     <>
@@ -28,9 +32,38 @@ const AdminPage = () => {
         <div key={user.id}>
           <h3>{user.username}</h3>
           <p>Role: {user.authorities[0].name}</p>
+          <UpdateRoleForm
+            userId={user.id}
+            userRole={user.authorities[0].name}
+            updateRole={updateRole}
+          />
         </div>
       ))}
     </>
+  );
+};
+
+type UpdateRoleFormProps = {
+  userId: number,
+  userRole: string,
+  updateRole: (id: number, role: string) => void
+};
+
+const UpdateRoleForm = ({ userId, userRole, updateRole }: UpdateRoleFormProps) => {
+  const defaultRole = userRole.split("_")[1].toLowerCase();
+  const [role, setRole] = useState<string>(defaultRole);
+
+  return (
+    <div className="form-group">
+      <label>Update Role: </label>
+      <select name="role" defaultValue={role} onChange={(e) => setRole(e.target.value)}>
+        <option></option>
+        <option value="user">User</option>
+        <option value="trainer">Trainer</option>
+        <option value="owner">Gym Owner</option>
+      </select>
+      <button disabled={defaultRole === "admin"} onClick={() => updateRole(userId, role)}>Update</button>
+    </div>
   );
 };
 
--- fitness-client/src/services/userService.ts
+++ fitness-client/src/services/userService.ts
@@ -4,10 +4,21 @@ export type User = {
   authorities: { name: string }[]
 };
 
-export const getUsers = async (token: String): Promise<User[]> => {
+export const getUsers = async (token: string): Promise<User[]> => {
   const res = await fetch("/api/users", {
     method: "GET",
     headers: { "Authorization": `Bearer ${token}` }
   });
   return res.json();
+};
+
+export const updateUserRole = async (userId: number, role: string, token: string): Promise<void> => {
+  await fetch(`/api/users/${userId}/update-role`, {
+    method: "PATCH",
+    headers: {
+      "Authorization": `Bearer ${token}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ role: `ROLE_${role.toUpperCase()}` })
+  });
 };
