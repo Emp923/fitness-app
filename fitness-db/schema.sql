a543b040993280f1cf75c55290c3ac29ef8e481c
Eric Penrod <emp923@gmail.com>
2025-03-11 14:18:49 -0400

    create programs table and relationships between programs exercies and users

--- fitness-db/schema.sql
+++ fitness-db/schema.sql
@@ -24,6 +24,37 @@ CREATE TABLE user_details (
     CONSTRAINT FK_user_details FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS trainer_details CASCADE;
+
+CREATE TABLE trainer_details (
+    user_id INT PRIMARY KEY,
+    name varchar(200),
+    bio varchar(255),
+    specialty_one varchar(50),
+    specialty_two varchar(50),
+    certification varchar(50),
+    photograph varchar(50),
+    CONSTRAINT FK_trainer_details FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS programs CASCADE;
+
+CREATE TABLE programs (
+    id SERIAL PRIMARY KEY,
+    name varchar(200),
+    created_by INT,
+    CONSTRAINT FK_programs FOREIGN KEY (created_by) REFERENCES trainer_details(user_id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS programs_users CASCADE;
+
+CREATE TABLE programs_users (
+    user_id INT,
+    program_id INT,
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    CONSTRAINT FK_program_id FOREIGN KEY (program_id) REFERENCES programs(id) ON DELETE CASCADE
+);
+
 DROP TABLE IF EXISTS program_exercises CASCADE;
 
 CREATE TABLE program_exercises (
@@ -37,6 +68,15 @@ CREATE TABLE program_exercises (
     CONSTRAINT FK_program_exercises FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS programs_program_exercises CASCADE;
+
+CREATE TABLE programs_program_exercises (
+    program_id INT,
+    program_exercise_id INT,
+    CONSTRAINT FK_program_id FOREIGN KEY (program_id) REFERENCES programs(id) ON DELETE CASCADE,
+    CONSTRAINT FK_program_exercise_id FOREIGN KEY (program_exercise_id) REFERENCES  program_exercises(id) ON DELETE CASCADE
+);
+
 DROP TABLE IF EXISTS exercise_log CASCADE;
 
 CREATE TABLE exercise_log (
@@ -47,19 +87,6 @@ CREATE TABLE exercise_log (
     repetitions INT,
     exercise_id INT,
     CONSTRAINT FK_exercise_log FOREIGN KEY (exercise_id) REFERENCES program_exercises(id) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS trainer_details CASCADE;
-
-CREATE TABLE trainer_details (
-    user_id INT PRIMARY KEY,
-    name varchar(200),
-    bio varchar(255),
-    specialty_one varchar(50),
-    specialty_two varchar(50),
-    certification varchar(50),
-    photograph varchar(50),
-    CONSTRAINT FK_trainer_details FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 COMMIT TRANSACTION;
