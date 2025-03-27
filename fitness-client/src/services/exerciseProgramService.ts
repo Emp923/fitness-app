4a4b22d92ca9fa361fc6a361092b221da35f9c71
Eric Penrod <emp923@gmail.com>
2025-03-27 15:26:27 -0400

    fixed spaces

--- fitness-client/src/services/exerciseProgramService.ts
+++ fitness-client/src/services/exerciseProgramService.ts
@@ -1,33 +1,33 @@
 export type ProgramExercise = {
-    id: number,
-    dayOfTheWeek: string,
-    exerciseName: string,
-    resistance: number,
-    sets: number,
-    repetitions: number
+  id: number,
+  dayOfTheWeek: string,
+  exerciseName: string,
+  resistance: number,
+  sets: number,
+  repetitions: number
 };
 
 export type ExerciseLog = {
-    id: number,
-    recordedDate: string,
-    resistance: number,
-    sets: number,
-    repetitions: number,
-    exerciseId: number
+  id: number,
+  recordedDate: string,
+  resistance: number,
+  sets: number,
+  repetitions: number,
+  exerciseId: number
 }
 
 export const getPrograms = async (token: string): Promise<ProgramExercise[]> => {
-    const res = await fetch("/api/programs/program-exercises", {
-        method: "GET",
-        headers: {"Authorization": `Bearer ${token}`}
-    });
-    return res.json();
+  const res = await fetch("/api/programs/program-exercises", {
+    method: "GET",
+    headers: {"Authorization": `Bearer ${token}`}
+  });
+  return res.json();
 };
 
 export const getExerciseLog = async (token: string): Promise<ExerciseLog[]> => {
-    const res = await fetch("/api/programs/exercise", {
-        method: "GET",
-        headers: {"Authorization": `Bearer ${token}`}
-    });
-    return res.json();
+  const res = await fetch("/api/programs/exercise", {
+    method: "GET",
+    headers: {"Authorization": `Bearer ${token}`}
+  });
+  return res.json();
 };
